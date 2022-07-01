2d725134b6191bb73203aecaa06f18a439450230
daggim99 <programmerdaggim99@gmail.com>
2022-07-01 19:51:55 +0300

    First

---
+++ client/src/data/MovieDataService.js
@@ -0,0 +1,18 @@
+export const genreData = [
+  'action',
+  'adventure',
+  'comedy',
+  'crime',
+  'drama',
+  'fantasy',
+  'historical',
+  'horror',
+  'mystery',
+  'romance',
+  'science fiction',
+  'thriller',
+  'political',
+  'western'
+];
+
+export const languageData = ['amharic','english', 'oromifa', 'somali', 'tigirena'];
